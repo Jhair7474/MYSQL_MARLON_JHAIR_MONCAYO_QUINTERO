2ac5f4fb1c5aafd56b4dd191e1e93420165071e2
Jhair7474 <marlonmoncayo7474@gmail.com>
2024-09-07 00:00:03 -0500

    Primer_commit

---
+++ Script/InsertUsuario_06-09-2024.sql
@@ -0,0 +1,7 @@
+use login_restaurante;
+
+INSERT INTO usuarios(id_Usuarios, nombre, apellido, email, contrasena) VALUES
+(1, 'Marlon', 'Moncayo', 'marlonmoncayo7474@gmail.com', '1122333444');
+
+select * from usuarios
+
